2bc2ad1b1d8a6292c1dc501cb72f4c7aaafb8d97
Mike Aldred <mike.aldred@directcommunications.com.au>
2019-03-11 09:48:13 +0800

    No changes.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject au.com.directcommunications/tait-dmr "0.1.1-SNAPSHOT"
   :description "Functions related to handling specifics of the Tait DMR."
-  :url "http://example.com/FIXME"
+  :url "http://github.com/DirectCommunications/tait-dmr"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"
             :year 2017
             :key "mit"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
-                 [clj-time "0.11.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [clj-time "0.14.2"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}})
